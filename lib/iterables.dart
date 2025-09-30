f6c3d8101909d59d539acf4304be5f30e8b3b3c9
Ludovico Cammarata <cammarataludvico9@gmail.com>
2025-09-30 12:57:18 +0200

    Revert "Refactor iterable functions to use IList<Animal> for consistency and improve add/remove operations"
    This reverts commit d01e94cff96d00d48fad75f10503a597118ccfd4.

--- lib/iterables.dart
+++ lib/iterables.dart
@@ -1,38 +1,30 @@
-import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:its_aa_pn_2025_cross_platform/animal.dart';
 
-Iterable<Animal> getAnimalStartingWith(IList<Animal> animals) {
+Iterable<Animal> getAnimalStartingWith(Iterable<Animal> animals) {
   return animals.where((element) {
       return (element.name.toUpperCase()).startsWith("L");
     });
 }
 
-Iterable<Animal> addAnimal(IList <Animal> list, Animal animal) {
-  return list.add(animal);
+Iterable<Animal> addAnimal(List <Animal> list, Animal animal) {
+  list.add(animal);
+  return list;
 }
 
-Iterable<Animal> removeAnimal(IList <Animal> list, Animal animal) {
-  return list.remove(animal);
+Iterable<Animal> removeAnimal(List <Animal> list, Animal animal) {
+    list.remove(animal);
+    return list;
 }
 
-Iterable<Animal> updateAnimalName(IList <Animal> list, Animal oldAnimal, String newName) {
-// Rimuove oldAnimal e aggiunge uno nuovo con il nuovo nome
-
-  return list.replaceAllWhere(
-    (element) => element.name == oldAnimal.name,
-      Animal(
-        name: newName, 
-        bornAt: oldAnimal.bornAt, 
-        ownerName: oldAnimal.ownerName, 
-        lastVisitAt: oldAnimal.lastVisitAt)
-  );
-  /*list.remove(oldAnimal);
-  return list.add(
+Iterable<Animal> updateAnimalName(List <Animal> list, Animal oldAnimal, String newName) {
+  list.remove(oldAnimal); // muta solo la lista non lo cancella effettivamente, quindi hai i dati
+  list.add(
     Animal(
       name: newName, 
       bornAt: oldAnimal.bornAt, 
       ownerName: oldAnimal.ownerName, 
       lastVisitAt: oldAnimal.lastVisitAt
     )
-  );*/
+  );
+  return list;
 }
