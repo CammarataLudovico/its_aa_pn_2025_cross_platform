e9e5b03705f290ead411a460009e7722063a404c
Ludovico Cammarata <cammarataludvico9@gmail.com>
2025-10-07 10:28:46 +0200

    Update requirements and enhance Handler class structure

--- REQUIREMENTS.md
+++ REQUIREMENTS.md
@@ -6,3 +6,4 @@
 4. togliere l'animale dalla mia clinica (es. trasferimento)
 5. modificare un padrone perchè ha aggiornato qualche suo dato
 6. un animale può cambiare padrone
+7. sono interessato alla lista di clienti
--- lib/handler.dart
+++ lib/handler.dart
@@ -1,3 +1,12 @@
+import 'package:clinic/animal.dart';
+import 'package:clinic/person.dart';
+
 class Handler {
-  
+  List<Animal> animals = [];
+
+  List<Person> get people {
+    animals.map(
+      (animal) => animal.owner,
+    ).toList();
+  }  
 }
